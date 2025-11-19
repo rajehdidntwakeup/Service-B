b58bcc0cc7e2aa60c3f55a512d3d7882cb7f6da4
Rajehdidntwakeup <rajeh3010@gmail.com>
2025-11-19 20:56:34 +0100

    chore(test): add documentation to `contextLoads` test in Service B

--- src/test/java/test/serviceb/ServiceBApplicationTests.java
+++ src/test/java/test/serviceb/ServiceBApplicationTests.java
@@ -6,8 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServiceBApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  /**
+   * Tests whether the application context loads successfully.
+   * This method ensures that the Spring application context
+   * is properly initialized without any issues.
+   */
+  @Test
+  void contextLoads() {
+  }
 
 }
